407179b4b9b775d3664778c20b4f6c3226382943
KillianVitug <killianjarelvitug@yahoo.com>
2023-09-29 12:04:55 +0800

    first commit

---
+++ PDC60Mod02Act02/PDC60Mod02Act02/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PDC60Mod02Act02
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ClickedCompass(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new View.Compass());
+        }
+    }
+}
